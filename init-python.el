6527ed939095853481e778e7a2d97946d3d8bbde
Nick Pascucci <npascut1@gmail.com>
2011-09-17 01:47:01 -0600

    Moved things around, some tweaks to try and get autocomplete working.

--- init-python.el
+++ init-python.el
@@ -1,56 +1,78 @@
-(require 'ipython)
-(require 'python-pylint)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Python mode customizations
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Custom python mode
+(add-to-list 'load-path "~/.emacs.d/dave-loves-python-mode")
+(load-library "python")
+
+(require 'python)
+(require 'auto-complete)
+(require 'yasnippet)
+
 (autoload 'python-mode "python-mode" "Python Mode." t)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
+(setq interpreter-mode-alist
+      (cons '("python" . python-mode)
+	    interpreter-mode-alist)
+      python-mode-hook
+      '(lambda () (progn
+		    (set-variable 'py-indent-offset 2)
+		    (set-variable 'py-smart-indentation nil)
+		    (set-variable 'indent-tabs-mode nil) 
+		    ;;(highlight-beyond-fill-column)
+                    (define-key python-mode-map "\C-m" 'newline-and-indent)
+		    ;(pabbrev-mode)
+		    ;(abbrev-mode)
+	 )
+      )
+)
 
-;; Initialize Pymacs
+;; Autofill inside of comments
+
+(defun python-auto-fill-comments-only ()
+  (auto-fill-mode 1)
+  (set (make-local-variable 'fill-nobreak-predicate)
+       (lambda ()
+         (not (python-in-string/comment)))))
+(add-hook 'python-mode-hook
+          (lambda ()
+            (python-auto-fill-comments-only)))
+
+;; pymacs
 (autoload 'pymacs-apply "pymacs")
 (autoload 'pymacs-call "pymacs")
 (autoload 'pymacs-eval "pymacs" nil t)
 (autoload 'pymacs-exec "pymacs" nil t)
 (autoload 'pymacs-load "pymacs" nil t)
-(eval-after-load "pymacs"
-  '(add-to-list 'pymacs-load-path "./pymacs"))
-
-;; Initialize Rope
 (pymacs-load "ropemacs" "rope-")
 (setq ropemacs-enable-autoimport t)
 
-(add-hook 'python-mode-hook
-      (lambda ()
-	(set-variable 'py-indent-offset 2)
-	;(set-variable 'py-smart-indentation nil)
-	(set-variable 'indent-tabs-mode nil)
-	(define-key py-mode-map (kbd "RET") 'newline-and-indent)
-	(define-key py-mode-map [tab] 'yas/expand)
-	(setq yas/after-exit-snippet-hook 'indent-according-to-mode)
-	(smart-operator-mode-on)
-	))
+;; Initialize Yasnippet                                                                                        
+;Don't map TAB to yasnippet                                                                                    
+;In fact, set it to something we'll never use because                                                          
+;we'll only ever trigger it indirectly.                                                                        
+(setq yas/trigger-key (kbd "C-c <kp-multiply>"))
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/snippets")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Ryan's Auto-completion
+;;; Auto-completion
 ;;;  Integrates:
 ;;;   1) Rope
 ;;;   2) Yasnippet
 ;;;   all with AutoComplete.el
-;;; Source: http://www.enigmacurry.com/2009/01/21/autocompleteel-python-code-completion-in-emacs/
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (defun prefix-list-elements (list prefix)
   (let (value)
     (nreverse
      (dolist (element list value)
       (setq value (cons (format "%s%s" prefix element) value))))))
-
 (defvar ac-source-rope
   '((candidates
      . (lambda ()
          (prefix-list-elements (rope-completions) ac-target))))
   "Source for Rope")
-
 (defun ac-python-find ()
   "Python `ac-find-function'."
   (require 'thingatpt)
@@ -60,7 +82,6 @@
             (point)
           nil)
       symbol)))
-
 (defun ac-python-candidate ()
   "Python `ac-candidates-function'"
   (let (candidates)
@@ -82,50 +103,82 @@
             (setcdr (nthcdr (1- ac-limit) cand) nil))
         (setq candidates (append candidates cand))))
     (delete-dups candidates)))
-
 (add-hook 'python-mode-hook
           (lambda ()
                  (auto-complete-mode 1)
                  (set (make-local-variable 'ac-sources)
-                      (append ac-sources '(ac-source-rope) '(ac-source-yasnippet)))
+                      (append ac-sources '(ac-source-rope)))
                  (set (make-local-variable 'ac-find-function) 'ac-python-find)
                  (set (make-local-variable 'ac-candidate-function) 'ac-python-candidate)
                  (set (make-local-variable 'ac-auto-start) nil)))
 
-;; Ryan's Python specific tab completion
-
-(defun ryan-python-tab ()
-  ; Try the following:
-  ; 1) Do a yasnippet expansion
-  ; 2) Do a Rope code completion
-  ; 3) Do an indent
+;;Ryan's python specific tab completion
+  ; Try the following in order:
+  ; 1) Try a yasnippet expansion without autocomplete
+  ; 2) If at the beginning of the line, indent
+  ; 3) If at the end of the line, try to autocomplete
+  ; 4) If the char after point is not alpha-numerical, try autocomplete
+  ; 5) Try to do a regular python indent.
+  ; 6) If at the end of a word, try autocomplete.
+(define-key python-mode-map "\t" 'yas/expand)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (set (make-local-variable 'yas/trigger-fallback) 'ryan-python-expand-after-yasnippet)))
+(defun ryan-indent ()
+  "Runs indent-for-tab-command but returns t if it actually did an indent; nil otherwise"
+  (let ((prev-point (point)))
+    (indent-for-tab-command)
+    (if (eql (point) prev-point)
+        nil
+      t)))
+(defun ryan-python-expand-after-yasnippet ()
   (interactive)
-  (if (eql (ac-start) 0)
-      (indent-for-tab-command)))
+  ;;2) Try indent at beginning of the line
+  (let ((prev-point (point))
+        (beginning-of-line nil))
+    (save-excursion
+      (move-beginning-of-line nil)
+      (if (eql 0 (string-match "\\W*$" (buffer-substring (point) prev-point)))
+          (setq beginning-of-line t)))
+    (if beginning-of-line
+        (ryan-indent)))
+  ;;3) Try autocomplete if at the end of a line, or
+  ;;4) Try autocomplete if the next char is not alpha-numerical
+  (if (or (string-match "\n" (buffer-substring (point) (+ (point) 1)))
+          (not (string-match "[a-zA-Z0-9]" (buffer-substring (point) (+ (point) 1)))))
+      (ac-start)
+    ;;5) Try a regular indent
+    (if (not (ryan-indent))
+        ;;6) Try autocomplete at the end of a word
+        (if (string-match "\\W" (buffer-substring (point) (+ (point) 1)))
+            (ac-start)))))
+
+;; End Tab completion
+
+
+;; Workaround so that Autocomplete is by default is only invoked explicitly,
+;; but still automatically updates as you type while attempting to complete.
 (defadvice ac-start (before advice-turn-on-auto-start activate)
   (set (make-local-variable 'ac-auto-start) t))
 (defadvice ac-cleanup (after advice-turn-off-auto-start activate)
   (set (make-local-variable 'ac-auto-start) nil))
-(define-key py-mode-map "\t" 'ryan-python-tab)
+
+(define-key python-mode-map "\t" 'ryan-python-tab)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; End Auto Completion
-;;; End Ryan's customizations
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Auto Syntax Error Hightlighting
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-		       'flymake-create-temp-inplace))
-	   (local-file (file-relative-name
-			temp-file
-			(file-name-directory buffer-file-name))))
-      (list "pyflakes" (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-	       '("\\.py\\'" flymake-pyflakes-init)))
+;;Autofill comments
+;;TODO: make this work for docstrings too.
+;;      but docstrings just use font-lock-string-face unfortunately
+(add-hook 'python-mode-hook
+          (lambda ()
+            (auto-fill-mode 1)
+            (set (make-local-variable 'fill-nobreak-predicate)
+                 (lambda ()
+                   (not (eq (get-text-property (point) 'face)
+                            'font-lock-comment-face))))))
 
-(add-hook 'find-file-hook 'flymake-find-file-hook)
-(add-hook 'python-mode-hooks 'python-load-libs)
-(global-set-key "\C-c \C-e" 'flymake-display-err-for-current-line)
-(provide 'init_python)
+;; (brm-init)
+;; (require 'pycomplete)
