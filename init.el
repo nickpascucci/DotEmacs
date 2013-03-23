566a7c31d185a33a1b3841d00e13fa992cf669cf
Nick Pascucci <npascut1@gmail.com>
2013-03-23 13:43:01 -0400

    Update load paths to use relative dirs.

--- init.el
+++ init.el
@@ -4,20 +4,22 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
+(add-to-list 'load-path "~/.emacs.d/extras")
+(require 'nick-custom)
+
 ;; Load directories and custom elisp files.
 (add-to-list 'load-path "~/.emacs.d/")
-(add-to-list 'load-path "~/.emacs.d/cedet-1.1/")
-(let ((default-directory "~/.emacs.d"))
-  (normal-top-level-add-subdirs-to-load-path))
-(let ((default-directory "~/.emacs.d/vendor/"))
-  (normal-top-level-add-subdirs-to-load-path))
+(np/prepend-subdirs "~/.emacs.d")
+(np/prepend-subdirs "~/.emacs.d/cedet-1.1/")
+(np/prepend-subdirs "~/.emacs.d/vendor/")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/vendor")
 
 ;; Load custom elisp as a dependency for various other customizations.
-(require 'nick-custom)
+
+(require 'org)
 
 ;; Automatically tangle (extract source) and load up all literate org-mode files in this directory.
-(setq dotfiles-dir "/home/nick/.emacs.d/")
+(setq dotfiles-dir "~/.emacs.d/")
 (setq org-custom-library-dir (expand-file-name "extras" dotfiles-dir))
 (mapc #'org-babel-load-file (directory-files org-custom-library-dir t "\\.org$"))
 (mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
