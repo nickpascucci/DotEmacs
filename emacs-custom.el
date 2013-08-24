69b5114a7c635b1fd974c0c3f47b20c157750f46
Nick Pascucci <npascut1@gmail.com>
2013-08-24 18:44:09 -0400

    Customize helm-for-files sources.

--- emacs-custom.el
+++ emacs-custom.el
@@ -20,6 +20,7 @@
  '(fringe-mode 4 nil (fringe))
  '(gnus-default-nntp-server "news.gmane.org")
  '(gnus-use-full-window nil)
+ '(helm-for-files-preferred-list (quote (helm-source-buffers-list helm-source-files-in-current-dir helm-source-recentf helm-source-file-cache helm-source-bookmarks helm-source-locate)))
  '(jabber-alert-info-message-hooks (quote (jabber-info-echo)))
  '(jabber-alert-presence-hooks nil)
  '(jabber-chat-buffer-show-avatar nil)
