3b7b4ff32975b8ac0474a070b1d4a25bb3afe4de
Nick Pascucci <npascut1@gmail.com>
2013-08-24 16:18:27 -0400

    Take out the trash - remove packages from vendor/ that are in package archives.

--- vendor/helm/helm-pkg.el
+++
@@ -1,9 +0,0 @@
-;;; helm-pkg.el --- define helm for package.el
-
-(define-package "helm" "1.5.1"
-  "Helm is an Emacs incremental and narrowing framework")
-
-;; Local Variables:
-;; byte-compile-warnings: (not cl-functions obsolete)
-;; no-byte-compile: t
-;; End:
